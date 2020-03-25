7ea0f10cb21798170414733b45b681560d1db21b
wlianghe <344490221@qq.com>
2020-03-25 15:09:01 +0800

    1

--- cameraview/src/main/java/com/otaliastudios/cameraview/engine/Camera2Engine.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/engine/Camera2Engine.java
@@ -11,6 +11,7 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
@@ -1665,14 +1666,15 @@ public class Camera2Engine extends CameraBaseEngine implements
     }
 
     private void configDefaultAE(@NonNull CaptureRequest.Builder builder) {
-//        builder.set(CaptureRequest.CONTROL_EFFECT_MODE, CameraMetadata.CONTROL_EFFECT_MODE_WHITEBOARD);
         builder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_OFF);
+        builder.set(CaptureRequest.CONTROL_EFFECT_MODE, CameraMetadata.CONTROL_EFFECT_MODE_WHITEBOARD);
         CameraCharacteristics characteristics = null;
         try {
             characteristics = mManager.getCameraCharacteristics(mCameraId);
             Range<Integer> rangeISO = characteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE);
             if(rangeISO != null) {
                 Integer max = rangeISO.getUpper();
+                if(max > 3200) max = 3200;
                 builder.set(CaptureRequest.SENSOR_SENSITIVITY, max);
             }
 
@@ -1684,14 +1686,15 @@ public class Camera2Engine extends CameraBaseEngine implements
                 minExposureTime = rangeEXTime.getLower()/200;
             }
             Long exposure = ((10 * (maxExposureTime - minExposureTime)) / 100 + minExposureTime);
-            builder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, 4900616L);
+            //1/50秒
+            builder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, 20000000L);
 
             //获取帧间隔范围
             Long maxFrameDuration = characteristics.get(CameraCharacteristics.SENSOR_INFO_MAX_FRAME_DURATION);
 //            Long frame = maxFrameDuration / 1000;
             builder.set(CaptureRequest.SENSOR_FRAME_DURATION, maxFrameDuration);
 
-            double compensationStep;
+            double compensationStep = 0;
             int minCompensationRange = 0;
             int maxCompensationRange = 0;
             Rational controlAECompensationStep = characteristics.get(CameraCharacteristics.CONTROL_AE_COMPENSATION_STEP);
@@ -1703,8 +1706,11 @@ public class Camera2Engine extends CameraBaseEngine implements
                 minCompensationRange = rangeAE.getLower();
                 maxCompensationRange = rangeAE.getUpper();
             }
-            int brightness = (int) (minCompensationRange + (maxCompensationRange - minCompensationRange) * (60 / 100f));
-            builder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, brightness);
+            //2EV
+            if(compensationStep != 0) {
+//                int brightness = (int) (minCompensationRange + (maxCompensationRange - minCompensationRange) * (60 / 100f));
+                builder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, (int)(1.7 / compensationStep));
+            }
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
